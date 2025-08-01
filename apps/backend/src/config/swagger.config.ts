8a343fa9a1431c6a1a75c456abc4a353165468e0
Danmo Ariel <danmoar.dev@proton.me>
2025-08-01 11:45:05 +0000

    feat(backend): add swagger docs

---
+++ apps/backend/src/config/swagger.config.ts
@@ -0,0 +1,48 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+interface DocMetadata {
+  title: string;
+  description: string;
+  version?: string;
+  globalPath: string;
+}
+
+export const swaggerSetup = ({
+  app,
+  metadata: { title, description, globalPath, version = '1.0' },
+}: {
+  app: INestApplication;
+  metadata: DocMetadata;
+}) => {
+  const options = new DocumentBuilder()
+    .setTitle(title)
+    .setDescription(description)
+    .setVersion(version)
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        description: 'Enter JWT token',
+      },
+      'JWT-auth',
+    )
+    .addSecurity('optional-auth', {
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      description: 'Optional JWT token for enhanced features',
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup(`${globalPath}/docs`, app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
+};
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  API_PREFIX,
+  API_VERSION,
+  APP_DESCRIPTION,
+  APP_NAME,
+} from './common/constants';
+import { swaggerSetup } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix(`${API_PREFIX}/`);
+
+  // Redirect
+  app.getHttpAdapter().get('', (req, res) => {
+    res.redirect(`${API_PREFIX}/docs`);
+  });
+
+  // Setup Swagger
+  swaggerSetup({
+    app,
+    metadata: {
+      title: APP_NAME,
+      description: APP_DESCRIPTION,
+      globalPath: API_PREFIX,
+      version: API_VERSION,
+    },
+  });
+
+  // Redirect to Swagger API
+
   await app.listen(process.env.PORT ?? 7777);
 }
 void bootstrap();
