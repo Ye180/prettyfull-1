04e7e675f94d1eb7b18cd507539e0e6c90690b9b
Emmanuel252 <ouattarayoussoufemmanuel@gmail.com>
2025-08-17 19:13:19 +0000

    Setting up of the other part for the grid of product card part second part

--- apps/web/src/app/(home)/page.tsx
+++ apps/web/src/app/(home)/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, CardProduct, GridCardProduct } from "@prettyfull/ui";
+import { Button } from "@prettyfull/ui";
 
 const page = () => {
   return (
@@ -27,35 +27,6 @@ const page = () => {
             <Button>Get Started</Button>
           </div>
         </div>
-      </div>
-      <div className="w-full flex justify-between  max-lg:flex-col p-4 bg-gradient-to-br from-slate-50 to-slate-100 lg:px-[8rem]">
-        <GridCardProduct className="" action_grid={true}>
-          <>
-            {Array.from({ length: 15 }).map((_, i) => (
-              <div key={i} className=" w-full aspect-10/9 ">
-                <CardProduct
-                  notVariable={{
-                    color: {
-                      code: "#FF0000",
-                      label: "Rouge",
-                    },
-                    image: "/assets/product_1.jpg",
-                    quantity: 1,
-                    size: ["S", "M", "L"],
-                  }}
-                  onClick={() => {}}
-                  price={12000}
-                  promotion={{
-                    pourcentage: 50,
-                    reduced_price: 6000,
-                  }}
-                  small_description="Top polyvalente á Manche"
-                  title="Sweet-Top"
-                />
-              </div>
-            ))}
-          </>
-        </GridCardProduct>
       </div>
     </>
   );
