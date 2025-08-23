e2443be5e1bd73c21f0aa95fcce4a6312c6e7269
Danmo Ariel <danmoar.dev@proton.me>
2025-08-23 12:33:49 +0000

    feat: add error feedback icon , layout helper component and AuthLayout

--- packages/ui/src/card-product.tsx
+++ packages/ui/src/card-product.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { cn, data_url, formatCurrency_FR } from "@prettyfull/utils";
-import { cva, VariantProps } from "class-variance-authority";
+import { VariantProps, cva } from "class-variance-authority";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { Button } from "./button";
-import { AddToCardIcon } from "./icons/add.cart.icon";
+import { AddToCardIcon } from "./icons/add-cart.icon";
 import { Heart } from "./icons/heart.icon";
 
 const cardVariants = cva(["space-y-3 w-[100%] h-full relative"], {
