1cf8b97f5bb0949014ac4f43e536c95af711a086
Emmanuel252 <ouattarayoussoufemmanuel@gmail.com>
2025-10-19 18:55:51 +0000

    Mise en place de la cart product

--- apps/web/src/shared/components/molecules/core/search.tsx
+++ apps/web/src/shared/components/molecules/core/search.tsx
@@ -6,10 +6,7 @@ import Container from "../../../../../../../packages/ui/src/layouts/helpers/cont
 const SearchBar = () => {
 	const t = useTranslations("HomePage.header");
 	return (
-		<Container maxWidth="100vw" className="px-4 ">
-			{/* <div className="items-center justify-between w-full h-fit sm:hidden max-sm:flex max-sm:gap-x-2 "> */}
-			{/* <div className="items-center  w-[100%] gap-2 py-1 text-gray-500 border-b border-gray-300 sm:hidden outline-gray-700 max-sm:flex bg-amber-100  flex justify-between"> */}
-			{/* <Search className="" /> */}
+		<Container maxWidth="100vw" className="px-4 max-sm:flex sm:hidden ">
 			<div className="flex items-center justify-center w-full gap-2 pb-2 border-b border-gray-300 ">
 				<Search className="opacity-50" />
 				<div className="w-full ">
@@ -18,14 +15,7 @@ const SearchBar = () => {
 						className="h-4 flex-1  border-none outline-1  px-0 text-black font-light  py-4  border-gray-300 focus:ring-0 focus:border-none  text-[1.8rem] max-md:flex  placeholder:font-light placeholder:text-gray-400 placeholder:text-[1.5rem] "
 					/>
 				</div>
-				{/* <Input
-					placeholder={t("placeholder")}
-					className="h-4 flex-1  border-none outline-1  px-0 text-black font-light  py-4  border-gray-300 focus:ring-0 focus:border-none  text-[1.8rem] max-md:flex  placeholder:font-light placeholder:text-gray-400 placeholder:text-[1.5rem] "
-				/> */}
 			</div>
-
-			{/* </div> */}
-			{/* </div> */}
 		</Container>
 	);
 };
