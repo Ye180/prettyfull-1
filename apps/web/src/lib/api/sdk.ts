6f4a8eca7f56ae40a253b72ded678f6bfe400b24
Ouattara Youssouf Emmanuel <ouattarayoussoufemmanuel@gmail.com>
2025-12-10 14:57:57 +0000

    Mise a jour de l'environement

--- apps/web/src/lib/api/sdk.ts
+++ apps/web/src/lib/api/sdk.ts
@@ -1,10 +1,6 @@
 import Medusa from "@medusajs/js-sdk"
 
-let MEDUSA_BACKEND_URL = "http://localhost:9000"
-
-if (process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL) {
-  MEDUSA_BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL
-}
+const MEDUSA_BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL || "http://localhost:9000"
 
 export const sdk = new Medusa({
   baseUrl: MEDUSA_BACKEND_URL,auth: {
