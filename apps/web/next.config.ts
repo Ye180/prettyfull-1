6d479d861d7d51f0b30e2005c81ee1d14d662414
Danmo Ariel <danmoar.dev@proton.me>
2026-01-29 11:18:45 +0000

    fix: try to fix module not found

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,7 +4,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  transpilePackages: ["@prettyfull/ui"],
+  transpilePackages: ["@prettyfull/ui", "@prettyfull/store"],
   images: {
     remotePatterns: [
       // {
