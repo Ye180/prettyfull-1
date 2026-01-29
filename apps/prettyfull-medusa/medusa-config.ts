bf82700540d480c1843f3c7bf0baad410755e788
Danmo Ariel <danmoar.dev@proton.me>
2026-01-29 21:02:07 +0000

    fix: medusa ssl block migrate

--- apps/prettyfull-medusa/medusa-config.ts
+++ apps/prettyfull-medusa/medusa-config.ts
@@ -1,24 +1,27 @@
-import { defineConfig, loadEnv } from '@medusajs/framework/utils';
-import "./scripts/seed";
+import { defineConfig, loadEnv } from "@medusajs/framework/utils";
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
+    databaseDriverOptions: {
+      ssl: false,
+    },
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
   },
-   modules: [
+  modules: [
     {
       resolve: "./src/modules/product-media",
-     },
-     {
+    },
+    {
       resolve: "@medusajs/medusa/payment",
       options: {
         providers: [
@@ -33,9 +36,5 @@ module.exports = defineConfig({
         ],
       },
     },
-
   ],
-   
-   
-
-})
+});
