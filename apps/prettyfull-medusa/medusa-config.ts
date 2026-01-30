3c66f952c2552e4a9616e5c8a9bd92ff179469d2
Ouattara Youssouf Emmanuel <ouattarayoussoufemmanuel@gmail.com>
2026-01-30 15:30:03 +0000

    Mise en palce de prettyfull-backend

--- apps/prettyfull-medusa/medusa-config.ts
+++ apps/prettyfull-medusa/medusa-config.ts
@@ -3,12 +3,7 @@ import { defineConfig, loadEnv } from "@medusajs/framework/utils";
 loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
-  plugins: [
-    {
-      resolve: "medusa-plugin-wishlist",
-      options: {},
-    },
-  ],
+ 
 
   
   projectConfig: {
