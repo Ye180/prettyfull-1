f508c5c7110eab8d23f881a1bf3c65288eb61b47
Ouattara Youssouf Emmanuel <ouattarayoussoufemmanuel@gmail.com>
2026-03-21 13:09:08 +0000

    Configuration des CORS pour le domaine dev.prettyfull.shop 2

--- apps/web/src/lib/api/sdk.ts
+++ apps/web/src/lib/api/sdk.ts
@@ -1,13 +1,13 @@
 import Medusa from "@medusajs/js-sdk"
 
-const MEDUSA_BACKEND_URL ="https://admin.prettyfull.shop/"
+const MEDUSA_BACKEND_URL ="http://localhost:9000/"
 
 export const sdk = new Medusa({
   baseUrl: MEDUSA_BACKEND_URL,
   auth: {
     type: "jwt",
   },
-  publishableKey: process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY,
+  publishableKey: "pk_1528422233a9c4c28cf0560e3270f8072fda717ed5793a9e2f5e57ea8ee41f46",
   globalHeaders: {
     "Access-Control-Allow-Origin": "*",
   },
@@ -18,7 +18,7 @@ export const sdkStore = new Medusa({
   auth: {
     type: "session",
   },
-  publishableKey: process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY,
+  publishableKey: "pk_1528422233a9c4c28cf0560e3270f8072fda717ed5793a9e2f5e57ea8ee41f46",
   globalHeaders: {
     "Access-Control-Allow-Origin": "*",
   },
