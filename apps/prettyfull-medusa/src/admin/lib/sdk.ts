4fabf46600ab971b0992e395445affb32fcc2471
Ouattara Youssouf Emmanuel <ouattarayoussoufemmanuel@gmail.com>
2026-04-09 10:37:08 +0000

    Mise à jour de l'URL backend de localhost vers admin.prettyfull.shop dans la configuration et le SDK

--- apps/prettyfull-medusa/src/admin/lib/sdk.ts
+++ apps/prettyfull-medusa/src/admin/lib/sdk.ts
@@ -1,9 +1,9 @@
 import Medusa from "@medusajs/js-sdk"
 
-let MEDUSA_BACKEND_URL = "http://localhost:9000"
+let MEDUSA_BACKEND_URL = "https://admin.prettyfull.shop"
 
 export const sdk = new Medusa({
-  baseUrl: MEDUSA_BACKEND_URL || "/",
+  baseUrl: MEDUSA_BACKEND_URL,
   debug: process.env.NODE_ENV === "development",
   auth: {
     type: "session",
