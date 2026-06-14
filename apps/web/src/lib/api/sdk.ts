24884ad7931fe6b438db79dcfae8fff78a4ec833
Ouattara Youssouf Emmanuel <ouattarayoussoufemmanuel@gmail.com>
2026-06-14 23:09:06 +0000

    Mise a jour des dependances

--- apps/web/src/lib/api/sdk.ts
+++ apps/web/src/lib/api/sdk.ts
@@ -4,22 +4,22 @@ const MEDUSA_BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL
 const MEDUSA_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY
 
 export const sdk = new Medusa({
-  baseUrl: "https://admin.prettyfull.shop",
+  baseUrl: MEDUSA_BACKEND_URL!,
   auth: {
     type: "jwt",
   },
-  publishableKey: "pk_1528422233a9c4c28cf0560e3270f8072fda717ed5793a9e2f5e57ea8ee41f46",
+  publishableKey: MEDUSA_PUBLISHABLE_KEY!,
   globalHeaders: {
     "Access-Control-Allow-Origin": "*",
   },
 })
 
 export const sdkStore = new Medusa({
-  baseUrl: "https://admin.prettyfull.shop",
+  baseUrl: MEDUSA_BACKEND_URL!,
   auth: {
     type: "session",
   },
-  publishableKey:"pk_1528422233a9c4c28cf0560e3270f8072fda717ed5793a9e2f5e57ea8ee41f46",
+  publishableKey: MEDUSA_PUBLISHABLE_KEY!,
   globalHeaders: {
     "Access-Control-Allow-Origin": "*",
   },
