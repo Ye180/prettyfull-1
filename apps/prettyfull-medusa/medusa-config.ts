35ebd6a514aafe0412dc88bf0ed8e03c7f885958
Ouattara Youssouf Emmanuel <ouattarayoussoufemmanuel@gmail.com>
2026-07-09 21:03:33 +0000

    Mise en place du stockage

--- apps/prettyfull-medusa/medusa-config.ts
+++ apps/prettyfull-medusa/medusa-config.ts
@@ -34,12 +34,12 @@ module.exports = defineConfig({
             resolve: "@medusajs/file-s3",
             id: "garage",
             options: {
-              file_url: process.env.GARAGE_PUBLIC_URL,
-              access_key_id: process.env.GARAGE_ACCESS_KEY,
-              secret_access_key: process.env.GARAGE_SECRET_KEY,
-              region: process.env.GARAGE_REGION,
-              bucket: process.env.GARAGE_BUCKET,
-              endpoint: process.env.GARAGE_ENDPOINT,
+              file_url: process.env.RUSTFS_PUBLIC_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT,
               additional_client_config: {
                 forcePathStyle: true,
               },
